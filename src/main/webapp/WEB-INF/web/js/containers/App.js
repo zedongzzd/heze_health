d27fbaa7b9c2404be2d33a82d607a98bc6833f90
guohao <wgh950509@gmail.com>
2016-03-10 16:16:49 +0800

    commit

---
+++ src/main/webapp/WEB-INF/web/js/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from "react"
+
+export default class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render(){
+    return (
+      <div>hello world</div>
+    )
+  }
+}
---
+++ src/main/webapp/WEB-INF/web/js/index.js
@@ -0,0 +1,16 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Router , hashHistory } from 'react-router'
+import createRoutes from './routes'
+import "./utils/setRem";
+import "./utils/dateExpand";
+
+
+window.onload = e =>{
+  document.body.style.minHeight=document.documentElement.clientHeight+"px";
+  render(
+    <Router history={hashHistory} routes={createRoutes()} />,
+    document.getElementById('root')
+  )
+}
