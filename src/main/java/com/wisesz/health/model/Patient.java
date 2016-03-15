71d990a93c5a3255e75c624a34acdf297efff1bf
zzd <Administrator@USER-20151003VX>
2016-03-15 09:37:04 +0800

    zzd

---
+++ src/main/java/com/wisesz/health/model/Patient.java
@@ -0,0 +1,62 @@
+package com.wisesz.health.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+import me.zzd.webapp.core.annotation.BindTable;
+
+@BindTable(tableName = "t_patient")
+public class Patient extends Model<Patient> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String getPatientId() {
+		return getStr("patientId");
+	}
+
+	public Patient setPatientId(String patientId) {
+		return set("patientId", patientId);
+	}
+
+	public String getCardNo() {
+		return getStr("cardNo");
+	}
+
+	public Patient setCardNo(String cardNo) {
+		return set("cardNo", cardNo);
+	}
+
+	public String getName() {
+		return getStr("name");
+	}
+
+	public Patient setName(String name) {
+		return set("name", name);
+	}
+
+	public String getIdCard() {
+		return getStr("idCard");
+	}
+
+	public Patient setIdCard(String idCard) {
+		return set("idCard", idCard);
+	}
+
+	public Integer getType() {
+		return getInt("type");
+	}
+
+	public Patient setType(Integer type) {
+		return set("type", type);
+	}
+
+	public String getUid() {
+		return getStr("uid");
+	}
+
+	public Patient setUid(String uid) {
+		return set("uid", uid);
+	}
+}
