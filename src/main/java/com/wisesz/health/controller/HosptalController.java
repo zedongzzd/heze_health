1688f872c725ce186fbc7176ff59ce3c7fc590be
user.email <Administrator@2013-20131224ME>
2016-03-16 14:31:57 +0800

    3.16

--- src/main/java/com/wisesz/health/controller/HosptalController.java
+++ src/main/java/com/wisesz/health/controller/HosptalController.java
@@ -1,13 +1,21 @@
 package com.wisesz.health.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.annotation.PostConstruct;
 
 import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
 import com.jfinal.ext.interceptor.POST;
 import com.jfinal.log.Log;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
 import com.wisesz.health.common.Result.RespFactory;
+import com.wisesz.health.model.Dept;
 
 import me.zzd.webapp.core.annotation.BindController;
 
@@ -17,8 +25,8 @@ public class HosptalController extends Controller {
 
 	/**
 	 * 获取医院列表
+	 *
 	 */
-	@Before(POST.class)
 	public void list() {
 		try {
 			Map<String, Object> hospital = new HashMap<>();
@@ -29,5 +37,26 @@ public class HosptalController extends Controller {
 			log.error("获取医院列表出错！", e);
 			renderJson(RespFactory.isFail("获取医院列表出错！"));
 		}
+	}
+
+	/**
+	 * 分页获取科室
+	 */
+	public void deptlist() {
+		int page = 0;
+		int pageSize = 0;
+		if (getPara("len") != null) {
+			pageSize = Integer.parseInt(getPara("len"));
+		} else {
+			pageSize = 15;
+		}
+		if (getPara("num") != null) {
+			page = Integer.parseInt(getPara("num"));
+		} else {
+			page = 1;
+		}
+		String sql = "SELECT * FROM t_dept  ORDER BY deptId  ASC limit ?,?";
+		List<Dept> list = Dept.dao.find(sql, page, pageSize);
+		renderJson(RespFactory.isOk("获取科室分页列表成功！", list));
 	}
 }
